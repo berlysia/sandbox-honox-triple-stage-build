813656d5b6889766a8fb7da1c725e0cb755ac7e7
berlysia <berlysia@gmail.com>
2024-02-15 04:59:49 +0900

    patch to (at)hono/vite-cloudflare-pages

--- patches/@hono/vite-cloudflare-pages/dist/entry.js
+++ patches/@hono/vite-cloudflare-pages/dist/entry.js
@@ -28,9 +28,10 @@ import { serveStatic } from 'hono/cloudflare-pages'
 
 const worker = new Hono()
 worker.get('/favicon.ico', serveStatic())
-worker.get('/static/*', serveStatic())
 
 ${appStr}
+
+worker.get('/*', serveStatic())
 
 export default worker`;
 };
