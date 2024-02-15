1d12a52b3fea2e0bb1ab3dc24acfbd91d7308670
berlysia <berlysia@gmail.com>
2024-02-16 02:45:17 +0900

    it works

--- vite.config.ts
+++ vite.config.ts
@@ -3,22 +3,12 @@ import ssg from "@hono/vite-ssg";
 import honox from "honox/vite";
 import client from "honox/vite/client";
 import { defineConfig } from "vite";
+import devServer from "@hono/vite-dev-server";
+import { resolve } from "node:path";
 
 const entry = "app/server.ts";
 
 export default defineConfig(({ mode }) => {
-  // Triple stage build: client, ssg, worker
-
-  if (mode === "client") {
-    // Client build
-    return {
-      plugins: [client()],
-      build: {
-        outDir: ".hono",
-      },
-    };
-  }
-
   if (mode === "worker") {
     // Worker build
     return {
@@ -26,10 +16,31 @@ export default defineConfig(({ mode }) => {
     };
   }
 
+  if (mode === "client") {
+    // Client build
+    return {
+      plugins: [client()],
+      build: {
+        outDir: ".hono",
+        emptyOutDir: true,
+      },
+    };
+  }
+
   {
     // SSG build
     return {
-      plugins: [honox(), ssg({ entry })],
+      plugins: [
+        honox(),
+        devServer({ entry }),
+        ssg({ entry }),
+        {
+          name: "tweaks",
+          config() {
+            return { build: { emptyOutDir: false } };
+          },
+        },
+      ],
     };
   }
 });
